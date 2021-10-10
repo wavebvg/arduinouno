2efad781694efcf29d8e4fdeb22765326722b2c9
fa <wavebvg@gmail.com>
2021-10-10 22:46:48 +0300

    Улучшена программная реализация PDM для сервопривода

--- src/ServoI.pas
+++ src/ServoI.pas
@@ -190,10 +190,11 @@ asm
          STS     CycleTime,   R1
          STS     CycleTime+1, R1
          //    R20 := SortedServos[0].Counter - 2
-         LDI     R26, LO8(SortedServos)
-         LDI     R27, HI8(SortedServos)
-         ADIW    R26, 3
-         LD      R20, X
+         //LDI     R26, LO8(SortedServos)
+         //LDI     R27, HI8(SortedServos)
+         //ADIW    R26, 3
+         //LD      R20, X
+         LDS      R20, SortedServos + 3
          SBCI    R20, 2          
 {$IfDef USE_DEBUG_COUNTER}
          //    Запоминаем текущее значение счетчика для отладки
